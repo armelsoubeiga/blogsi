c11c48a792e12a0b50ea60752c0c7ef62db3d433
armelsoubeiga <38792575+armelsoubeiga@users.noreply.github.com>
2019-12-02 17:26:30 +0100

    Update urls.py

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('blog.urls')),
-    path('ckeditor/', include('ckeditor_uploader.urls')),
+    #path('ckeditor/', include('ckeditor_uploader.urls')),
     path('ckeditor/upload/', login_required(ckeditor_views.upload), name='ckeditor_upload'),
     path('ckeditor/browse/', never_cache(login_required(ckeditor_views.browse)), name='ckeditor_browse'),
 ]
